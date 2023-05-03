29ee57c1a1b721678ec6aadcc52a991d308bbb21
Sang-Hyeong Jin <jwjln3126@naver.com>
2023-05-03 17:47:27 +0900

    Update elan_mdict.py

--- tmp/elan_mdict.py
+++ tmp/elan_mdict.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 from yolov7.models.common import Conv, Concat
 
 class BBone_ELAN(nn.Module):
-    '''
-    Back Bone ELAN
-    '''
     def __init__(self, chan1, chan2, ker, depth):
         super(BBone_ELAN, self).__init__()
 
@@ -13,16 +10,15 @@ class BBone_ELAN(nn.Module):
         self.chan2 = chan2 # output channel size (= input_channel // 2)
         self.ker = ker # kernel size
         self.depth = depth # depth (could be variable)
-        
-        # index를 ELAN 정의할 때 parameter로 받으면?
+
         self.idx = [idx for idx in range(self.depth * 2) if (idx % 2 == 1 or idx == 0)] #include idx 0 always
 
         elans = {} # elan module dictionary
         for d in range(self.depth*2):
-            elans['elan_block_{0}'.format(d)] = self.construct_elan(d) # ELAN block consturct
+            elans['BBoneELAN_{0}'.format(d+1)] = self.construct_elan(d) # ELAN block consturct
 
-        self.elan_dict = nn.ModuleDict(elans) # constructed ELAN dictionary to ModuleDict
-        self.cat = Concat(dimension=1) # from yolov7 basic modules
+        self.elan_dict = nn.ModuleDict(elans) # ELAN module dictionary to ModuleDict
+        self.cat = Concat(dimension=1) # from yolov7 modules
 
     def construct_elan(self, depth):
         '''
@@ -31,29 +27,22 @@ class BBone_ELAN(nn.Module):
         elan = nn.Sequential()
 
         if depth == 0:
-            elan.add_module('conv_0', Conv(self.chan1, self.chan2, 1, 1))
+            elan.add_module('Conv_1', Conv(self.chan1, self.chan2, 1, 1))
 
         else:
-          if depth % 2 == 0:
             for i in range(depth):
               if i == 0:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan1, self.chan2, 1, 1))
+                elan.add_module('Conv_{0}'.format(i+2), Conv(self.chan1, self.chan2, 1, 1))
               else:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
-          else:
-            for i in range(depth):
-              if i == 0:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan1, self.chan2, 1, 1))
-              else:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
+                elan.add_module('Conv_{0}'.format(i+2), Conv(self.chan2, self.chan2, self.ker, 1))
+
 
         return elan
     
     def forward(self, x):
         tmp_out = []
         
-        # (idea?) idx에 해당하는것 먼저 select하고 for loop 돌리는게 나을지도?
+        # idx에 해당하는것 먼저 select하고 for문 돌리는게 나을 수도?
 
         for _, elan in self.elan_dict.items(): # get each elan module output
           tmp_out.append(elan(x))
@@ -81,7 +70,7 @@ class Head_ELAN(nn.Module):
 
         elans = {}
         for d in range(self.depth+1):
-            elans['elan_block_{0}'.format(d)] = self.construct_elan(d)
+            elans['HeadELAN_{0}'.format(d+1)] = self.construct_elan(d)
 
         self.elan_dict = nn.ModuleDict(elans)
         self.cat = Concat(dimension=1)
@@ -90,16 +79,16 @@ class Head_ELAN(nn.Module):
         elan = nn.Sequential()
 
         if depth == 0:
-            elan.add_module('conv_0', Conv(self.chan1, self.chan2, 1, 1))
+            elan.add_module('Conv_1', Conv(self.chan1, self.chan2, 1, 1))
 
         else:
           for i in range(depth):
             if i == 0:
-                elan.add_module('elan_conv_{0}'.format(i), Conv(self.chan1, self.chan2, 1, 1))
+                elan.add_module('Conv_{0}'.format(i+2), Conv(self.chan1, self.chan2, 1, 1))
             elif i == 1:
-                elan.add_module('elan_conv_{0}'.format(i), Conv(self.chan2, self._chan, self.ker, 1))
+                elan.add_module('Conv_{0}'.format(i+2), Conv(self.chan2, self._chan, self.ker, 1))
             else:
-                elan.add_module('elan_conv_{0}'.format(i), Conv(self._chan, self._chan, self.ker, 1))
+                elan.add_module('Conv_{0}'.format(i+2), Conv(self._chan, self._chan, self.ker, 1))
 
         return elan
     
