8f740cd0898618e509c8006343e30f6eda33274e
Sang-Hyeong Jin <jwjln3126@naver.com>
2023-05-03 15:30:18 +0900

    Update elan_mdict.py

--- tmp/elan_mdict.py
+++ tmp/elan_mdict.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 from yolov7.models.common import Conv, Concat
 
 class ELAN_block(nn.Module):
-    def __init__(self, chan, ker, depth):
+    def __init__(self, chan1, chan2, ker, depth):
         super(ELAN_block, self).__init__()
-        self.chan = chan
-        self.chan2 = chan//2
+        self.chan1 = chan1
+        #self.c_ = chan1 // 2
+        self.chan2 = chan2
 
         self.ker = ker
         self.depth = depth
+
+        self.act_idx = [i for i in range(self.depth * 2) if (i % 2 == 1 or i == 0)]
 
         # Declaration might be useless?
         # Conv() : ch_in, ch_out, kernel, stride=1, padding=none, groups=1
@@ -17,7 +20,7 @@ class ELAN_block(nn.Module):
         #self.conv2 = Conv(self.chan2, self.chan2, ker)
 
         elans = {}
-        for d in range(self.depth):
+        for d in range(self.depth*2):
             elans['elan_block_{0}'.format(d)] = self.construct_elan(d)
 
         self.elan_list = nn.ModuleDict(elans)
@@ -25,12 +28,25 @@ class ELAN_block(nn.Module):
 
     def construct_elan(self, depth):
         elan = nn.Sequential()
-
-        for i in range(depth):
-            if i == 0:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan, self.chan2, self.ker))
-            else:
-                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker))
+        #print('module',depth)
+        if depth == 0:
+            elan.add_module('conv_0', Conv(self.chan1, self.chan2, 1, 1))
+        else:
+          if depth % 2 == 0:
+            for i in range(depth):
+            #print('depth',depth, '/', i)
+              if i == 0:
+                elan.add_module('conv_{0}'.format(i), Conv(self.chan1, self.chan2, 1, 1))
+              else:
+                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
+          else:
+            for i in range(depth):
+            #print('depth',depth, '/', i)
+              if i == 0:
+                elan.add_module('conv_{0}'.format(i), Conv(self.chan1, self.chan2, 1, 1))
+              else:
+                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
+                elan.add_module('conv_{0}'.format(i), Conv(self.chan2, self.chan2, self.ker, 1))
 
         return elan
     
@@ -38,8 +54,11 @@ class ELAN_block(nn.Module):
         tmp_out = []
 
         for _, elan in self.elan_list.items():
+          #print('-----------')
           tmp_out.append(elan(x))
-
-        out = self.cat([tmp_out[d] for d in range(self.depth)])
+        
+        #print(len(tmp_out))
+        #out = self.cat([tmp_out[d] for d in range(self.depth*2)])
+        out = self.cat([tmp_out[d] for d in self.act_idx])
 
         return out
